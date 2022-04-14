bf4a33efc20da890bb6b91fa3c6c8412b30bcb49
DaoToanh <daotoanhktxd@gmail.com>
2022-04-14 23:46:09 +0700

    LAB05

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method
     if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message</title></head>')
@@ -9,7 +11,12 @@ const server = http.createServer((req, res) => {
         res.write('</html>')
         return res.end()
     }
-      console.log(req.url, req.method, req.headers)
+    if (url === '/message' && method === 'POST') {
+          fs.writeFileSync('message.txt', 'DUMMY')
+          res.statusCode = 302
+          res.setHeader('Location', '/')
+          return res.end()
+    }
       res.setHeader('Content-Type', 'text/html')
       res.write('<html>')
       res.write('<head><title>My First Page</title></head>')
