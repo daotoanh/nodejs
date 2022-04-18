0d6b9a1aaefe3fa2faf53609cc88e16f1e32b337
DaoToanh <daotoanhktxd@gmail.com>
2022-04-18 22:47:51 +0700

    LAB2.9

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -9,7 +8,7 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>')
