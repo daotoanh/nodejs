c0557099f63d42e236f7ed876507bbb88c1b6d78
DaoToanh <daotoanhktxd@gmail.com>
2022-04-28 00:49:03 +0700

    LAB4.21

--- models/cart.js
+++ models/cart.js
@@ -39,8 +39,10 @@ module.exports = class Cart {
           return;
         }
         const updatedCart = {...JSON.parse(fileContent)}
-        const product = updatedCart.products.findIndex(prod => prod.id !== id)
-       
+        const product = updatedCart.products.find(prod => prod.id === id)
+        if(!product) {
+          return;
+        }
         const productQty = product.qty
         updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
         updatedCart.totalPrice = updatedCart.productPrice - productPrice * productQty
