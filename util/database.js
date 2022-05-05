578ebef2c3f9a88f6398a7d5822ab79580fbed7c
DaoToanh <daotoanhktxd@gmail.com>
2022-05-06 00:33:50 +0700

    LAB6.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'lokebeoli', {
-    dialect: 'mysql',
-    host: 'localhost'
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://toanh:lokebeoli@cluster0.v2nz2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(client => {
+    console.log('Connected!')
+    callback(client)
 })
+.catch(err => console.log(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
