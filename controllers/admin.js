81951bbe72dacb77bc94d5d4894e75f3dafcabe3
DaoToanh <daotoanhktxd@gmail.com>
2022-05-07 21:36:48 +0700

    LAB6.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,4 @@
-const mongodb = require('mongodb')
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId
-
 
 //hien thi page them product
 exports.getAddProduct = (req, res, next) => {
@@ -66,7 +62,7 @@ exports.postEditProduct = (req, res, next) => {
     updatedPrice,
     updatedDesc,
     updatedImageUrl,
-    new ObjectId(prodId)
+    prodId
   );
   product
     .save()
