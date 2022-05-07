d77eadef49150b2d064d71bcf654cd4160dd5d9b
DaoToanh <daotoanhktxd@gmail.com>
2022-05-08 00:31:01 +0700

    LAB6.17

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('627696c8d0b49be049a8e237')
     .then(user => {
-       req.user = user
+       req.user =  new User(user.name, user.email, user.cart, user._id)
        next()
     })
     .catch(err => console.log(err))
