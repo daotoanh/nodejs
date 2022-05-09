d7379972a2a6a7a3b073ee6587b6b97dc8593b25
DaoToanh <daotoanhktxd@gmail.com>
2022-05-09 23:35:23 +0700

    LAB6.19

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,18 +53,14 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  console.log(req.user.cart)
-  req.user.getCart()
-    .then(cart => {
-      return cart.getProducts()
-        .then(products => {
+  req.user
+  .getCart()
+    .then(products => {
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
           });
-        })
-        .catch(err => console.log(err))
     }).catch(err => console.log(err))
   // Cart.getCart(cart => {
   //   Product.fetchAll(products => {
